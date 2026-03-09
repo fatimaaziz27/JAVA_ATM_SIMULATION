dac7809e7bc97503c8fd63dfe352412da2d2743f
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-03-10 04:05:50 +0500

    fix

--- main.java
+++ main.java
@@ -1,3 +1,36 @@
+// Output cases:
+// Welcome to Python Bank!
+
+// Signup
+// Login
+// Exit
+// Choose an option:
+
+// if Choose an option: 1
+
+// Enter Username:
+// Enter Password:
+// Set a 4-digit PIN:
+// Account Created Successfully!
+
+// Signup
+// login
+// Exit
+// Choose an option:
+
+// if Choose an option: 2
+// Enter Username:
+// Enter Password:
+// Login Successful!
+
+// Banking Menu:
+// Deposit Money
+// Withdraw Money
+// Check Balance
+// Transaction History !
+// Transfer Money
+// Logout
+
 import java.util.*;
 class main{
 
@@ -222,36 +255,3 @@ class account extends bank{
     }
 //     +===========+  CODE ENDS HERE  +===========+
 }
-
-// Output cases:
-// Welcome to Python Bank!
-
-// Signup
-// Login
-// Exit
-// Choose an option:
-
-// if Choose an option: 1
-
-// Enter Username:
-// Enter Password:
-// Set a 4-digit PIN:
-// Account Created Successfully!
-
-// Signup
-// login
-// Exit
-// Choose an option:
-
-// if Choose an option: 2
-// Enter Username:
-// Enter Password:
-// Login Successful!
-
-// Banking Menu:
-// Deposit Money
-// Withdraw Money
-// Check Balance
-// Transaction History !
-// Transfer Money
-// Logout
